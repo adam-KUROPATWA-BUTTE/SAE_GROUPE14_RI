c232d32fdaeece32dddc89dd18e1919063f2952e
Amine Nahi <amine.nahi@etu.univ-amu.fr>
2025-10-03 10:36:10 +0200

    premier essai avec la base de données

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+session_start(); // IMPORTANT : Ajoutez cette ligne au tout début si ce n'est pas déjà fait
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -8,57 +8,48 @@ error_reporting(E_ALL);
 $page = $_GET['page'] ?? 'home';
 
 switch ($page) {
-    case 'login':
-    case 'register':
-    case 'reset':
-        require_once __DIR__ . '/Controllers/AuthController.php';
-        $controller = new AuthController();
-        $controller->index();
-        break;
-    
-    case 'logout':
-        require_once __DIR__ . '/Controllers/AuthController.php';
-        $controller = new AuthController();
-        $controller->logout();
-        break;
-    
     case 'dashboard':
-        require_once __DIR__ . '/Controllers/DashboardController.php';
+        require_once __DIR__ . '/module/blog/Controllers/DashboardController.php';
         $controller = new DashboardController();
         $controller->index();
         break;
-    
-    case 'folders':
-        require_once __DIR__ . '/Controllers/FoldersController.php';
-        $controller = new FoldersController();
+
+    case 'login':
+    case 'register':
+    case 'reset':
+        require_once __DIR__ . '/module/blog/Controllers/AuthController.php';
+        $controller = new AuthController();
         $controller->index();
         break;
-    
+
     case 'settings':
-        require_once __DIR__ . '/Controllers/SettingsController.php';
+        require_once __DIR__ . '/module/blog/Controllers/SettingsController.php';
         $controller = new SettingsController();
         $controller->index();
         break;
-    
+
+    case 'folders':
+        require_once __DIR__ . '/module/blog/Controllers/FoldersController.php';
+        $controller = new FoldersController();
+        $controller->index();
+        break;
+
     case 'help':
-        require_once __DIR__ . '/Controllers/HelpController.php';
+        require_once __DIR__ . '/module/blog/Controllers/HelpController.php';
         $controller = new HelpController();
         $controller->index();
         break;
-    
+
     case 'web_plan':
-        require_once __DIR__ . '/Controllers/WebPlanController.php';
+        require_once __DIR__ . '/module/blog/Controllers/WebPlanController.php';
         $controller = new WebPlanController();
         $controller->index();
         break;
-    
+
     case 'home':
     default:
-        if (isset($_SESSION['admin_id'])) {
-            header('Location: index.php?page=dashboard');
-        } else {
-            header('Location: index.php?page=login');
-        }
-        exit;
+        require_once __DIR__ . '/module/blog/Controllers/IndexController.php';
+        $controller = new IndexController();
+        $controller->index();
         break;
 }
