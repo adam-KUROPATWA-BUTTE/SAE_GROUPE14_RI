ca34e93787adaa23a48c0eca9ebd3afe0876a28b
adam-KUROPATWA-BUTTE <adam13kuropatwa-butte@gmail.com>
2025-10-08 15:49:42 +0200

    ajout d'un Model\User;

--- module/blog/Controllers/AuthController.php
+++ module/blog/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 namespace Controllers\Blog;
 
 use Controllers\ControllerInterface;
+use Model\User;
 
 class AuthController implements ControllerInterface
 {
@@ -16,14 +17,12 @@ class AuthController implements ControllerInterface
         $isLogin = true;
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            require_once DIRECTORY_SEPARATOR . 'module' . DIRECTORY_SEPARATOR . 'blog' . DIRECTORY_SEPARATOR . 'Model' . DIRECTORY_SEPARATOR . 'User.php';
-            $action = $_POST['action'] ?? '';
-
+        
             if ($action === 'login') {
                 $email = $_POST['email'] ?? '';
                 $password = $_POST['password'] ?? '';
 
-                if (\Model\User::login($email, $password)) {
+                if (User::login($email, $password)) {
                     $_SESSION['admin_id'] = 1; // exemple
                     $_SESSION['message'] = "Connexion réussie !";
                     header('Location: /dashboard');
@@ -34,7 +33,7 @@ class AuthController implements ControllerInterface
             }
         }
 
-        require_once DIRECTORY_SEPARATOR . 'module' . DIRECTORY_SEPARATOR . 'blog' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'login.php';
+        require DIRECTORY_SEPARATOR . 'module' . DIRECTORY_SEPARATOR . 'blog' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'login.php';
     }
 
     public static function support(string $page, string $method): bool
