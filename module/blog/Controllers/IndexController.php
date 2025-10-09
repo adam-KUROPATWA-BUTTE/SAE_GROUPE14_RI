e2e0b218f23b9097e8e96c354eb9402e100ef068
adam-KUROPATWA-BUTTE <adam13kuropatwa-butte@gmail.com>
2025-10-09 18:38:03 +0200

    remise avanr chnagement

--- module/blog/Controllers/IndexController.php
+++ module/blog/Controllers/IndexController.php
@@ -1,5 +1,5 @@
 <?php
-namespace Controllers;
+namespace Controllers\Blog;
 
 use Controllers\ControllerInterface;
 use View\HomePage;
@@ -9,16 +9,8 @@ class IndexController implements ControllerInterface
     public function control(): void
     {
         $isLoggedIn = isset($_SESSION['admin_id']);
-        $percentage = $this->getCompletePercentage();
-
-        $view = new HomePage(isLoggedIn: $isLoggedIn, completePercentage: $percentage);
-        echo $view->render();
-    }
-
-    private function getCompletePercentage(): int
-    {
-        // TODO: récupérer depuis la base de données
-        return 0;
+        $view = new HomePage($isLoggedIn);
+        $view->render();
     }
 
     public static function support(string $page, string $method): bool
