c06dab4cf167091e173e7abdf70a745088daaa33
adam-KUROPATWA-BUTTE <adam13kuropatwa-butte@gmail.com>
2025-11-04 17:03:48 +0100

    changement du nom de domaine de provenence du mail et ajout du bon liens du logo amu

--- public/module/site/Service/Email/EmailReminderService.php
+++ public/module/site/Service/Email/EmailReminderService.php
@@ -6,7 +6,7 @@ use Mailjet\Resources;
 
 class EmailReminderService
 {
-    private static string $fromEmail = 'relance-iut@ri-amu.app';
+    private static string $fromEmail = 'relance-iut-amu@ri-amu.app';
     private static string $fromName = 'IUT Aix - Gestion Dossiers';
 
     public static function sendRelance(string $toEmail, int|string $dossierId, string $studentName = '', array $itemsToComplete = []): bool
@@ -77,7 +77,7 @@ class EmailReminderService
             $itemsHtml = '<p>Merci de compléter les pièces manquantes de votre dossier.</p>';
         }
 
-        $logoUrl = 'https://www.univ-amu.fr/sites/all/themes/amu/logo.png';
+        $logoUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBl1mF7ktLaJxYCRD64rZyUJ1WcUDvcJBcIw&s';
         $encodedId = urlencode((string)$dossierId);
         $link = "https://ri-amu.app/index.php?page=folders-student&action=view&id={$encodedId}";
 
