dd48fb89dccb93550826dc234fa6bbd6725e59a8
Amara <sinda.amara@etu.univ-amu.fr>
2026-01-06 11:36:32 +0100

    Separation la page Partenaires en deux pour chaque connexion

--- public/module/site/Model/WebPlan.php
+++ public/module/site/Model/WebPlan.php
@@ -12,8 +12,8 @@ class WebPlan
         $links = [
             ['url' => 'index.php?page=home', 'label' => 'Accueil'],
             ['url' => 'index.php?page=dashboard-admin', 'label' => 'Tableau de bord'],
-            ['url' => 'index.php?page=partners', 'label' => 'Partenaires'],
-            ['url' => 'index.php?page=folders', 'label' => 'Dossiers'],
+            ['url' => 'index.php?page=partners-admin', 'label' => 'Partenaires'],
+            ['url' => 'index.php?page=folders-admin', 'label' => 'Dossiers'],
             ['url' => 'index.php?page=login', 'label' => 'Connexion'],
 
         ];
