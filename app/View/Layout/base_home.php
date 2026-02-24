fd0bd378ab0d520627b1e0f8b75a4b52e925c0a9
Sinda Amara <sinda.amara@etu.univ-amu.fr>
2026-02-24 11:21:09 +0100

    Update base_home.php

--- app/View/Layout/base_home.php
+++ app/View/Layout/base_home.php
@@ -48,20 +48,8 @@ $isTritanopia = isset($_SESSION['tritanopia']) && $_SESSION['tritanopia'] === tr
 <?= $content ?>
 
 <?php include __DIR__ . '/chatbot.php'; ?>
+<?php include __DIR__ . '/footer.php'; ?>
 
-<!-- Footer inline pour home -->
-<footer>
-    <p>&copy; 2026 - Aix-Marseille Université</p>
-
-    <div class="footer-links">
-        <a href="https://www.instagram.com/relationsinternationales_amu/"
-           target="_blank"
-           rel="noopener noreferrer"
-           aria-label="Instagram">
-            <img class="insta" src="img/instagram.png" alt="Instagram">
-        </a>
-    </div>
-</footer>
 
 <script src="js/main.js"></script>
 <script src="js/chatbot.js"></script>
