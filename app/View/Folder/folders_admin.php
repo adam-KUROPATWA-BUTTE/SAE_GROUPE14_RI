7fba271f7b77cdc5bfaa09ed3db06a38413fa47c
mariampoghossian <poghossianm@gmail.com>
2026-03-11 18:29:38 +0100

    remplacement de 'Folder étudiant' pour 'Dossier étudiant'

--- app/View/Folder/folders_admin.php
+++ app/View/Folder/folders_admin.php
@@ -199,7 +199,7 @@ ob_start();
         include __DIR__ . '/../Partials/_banniere_modifie_par.php';
         ?>
 
-        <h1><?= $t(['fr' => 'Folder étudiant', 'en' => 'Student Profile']) ?></h1>
+        <h1><?= $t(['fr' => 'Dossier étudiant', 'en' => 'Student Profile']) ?></h1>
         <div class="form-back-button">
             <button onclick="window.location.href='<?= $buildUrl('index.php', ['page' => $PAGE]) ?>'" class="btn-secondary">
                 <?= $t(['fr' => 'Retour à la liste', 'en' => 'Back to List']) ?>
